79bc7dc407b46906c7abf0c597d48385a5747205
ken-peter <Ken Peter>
2017-03-14 15:21:18 +0300

    changed pictures icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // IDEA: script for index
 
 $(document).ready(function() {
-  $("img.landingpage").hover(function(event) {
-    (".other").slideUp();
+  $("img.landingpage").click(function(event) {
+    ("index.html").fadeOut(-90000);
+    ("contacts.html").fadeIn(-90000);
   });
 });
