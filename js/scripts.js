ac072ddde05eed30184528dcb66fba10dad8079a
ken-peter <Ken Peter>
2017-03-15 13:05:26 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,1 @@
 // IDEA: script for index
-
-$(document).ready(function() {
-  $("img.landingpage").click(function(event) {
-    ("index.html").fadeOut(-90000);
-    ("contacts.html").fadeIn(-90000);
-  });
-});
